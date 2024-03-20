fe3a13524693bb2c87cd563e068c3dd6f6e46e41
cracsi <98497136+cracsi@users.noreply.github.com>
2024-03-19 22:54:01 -0500

    Vista Login tereminada (visual)

---
+++ unimarket/lib/vistas/vista_home.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class VistaHome extends StatelessWidget {
+  const VistaHome({super.key});
+
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
