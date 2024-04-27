4efa91d3c2e46cc5ce0ddfa4b21bea8092ffd44d
cracsi <98497136+cracsi@users.noreply.github.com>
2024-04-27 05:03:11 -0500

    final sprint 3

--- unimarket/lib/Models/model.dart
+++ unimarket/lib/Models/model.dart
@@ -5,44 +5,13 @@ import 'package:unimarket/Models/product_model.dart';
 
 class Model {
   final productos = <ProductModel>[];
-  List<ProductModel> filteredProducts = <ProductModel>[];
-
   final productosCarrito = <ProductModel>[];
-
   var userId;
   var cartPrice = 0;
 
   static final Model single = Model._();
   factory Model() => single;
   Model._();
-
-
-  List<ProductModel> createList(){
-    List<ProductModel> lista = <ProductModel>[];
-    return lista;
-  }
-
-  getProducts() {
-    cargarProductos();
-    return productos;
-  }
-
-  getFilteredProducts(String category, bool use) async {
-    List<ProductModel> lista = <ProductModel>[];
-    await ProductRepository().getFilteredProducts(category, use, lista);
-    return lista;
-  }
-
-  addProduct(ProductModel p) {
-    productos.add(p);
-  }
-
-  addFilteredProduct(ProductModel product, List<ProductModel> lista){
-    lista.add(product);
-    //filteredProducts.add(product);
-  }
-
-  cargarProductos() {
 
   List<ProductModel> getProducts() {
     return productos;
@@ -70,12 +39,22 @@ class Model {
     productos.add(p);
   }
 
+  getFilteredProducts(String category, bool use) async {
+    List<ProductModel> lista = <ProductModel>[];
+    await ProductRepository().getFilteredProducts(category, use, lista);
+    return lista;
+  }
+
+  addFilteredProduct(ProductModel product, List<ProductModel> lista) {
+    lista.add(product);
+    //filteredProducts.add(product);
+  }
+
   void addProductToCart(String? pId) {
     CartRepository().addToCart(pId);
   }
 
   void cargarProductos() {
-
     ProductRepository().getData();
   }
 
