07ae5a34ecf3f4f656e038139dcf176b585410d6
cracsi <98497136+cracsi@users.noreply.github.com>
2024-05-27 00:03:40 -0500

    modifications

--- unimarket/lib/Views/login_view.dart
+++ unimarket/lib/Views/login_view.dart
@@ -293,6 +293,7 @@ class _LoginViewState extends State<LoginView> {
                         ElevatedButton(
                           onPressed: () {
                             // Add your registration logic here
+                            FlutterIsolate.killAll();
                             Navigator.push(
                                 context,
                                 MaterialPageRoute(
